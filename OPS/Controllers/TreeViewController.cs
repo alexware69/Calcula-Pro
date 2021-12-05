e73720018d4deb14b26a1387b129907d8b379996
Alejandro Penate-Diaz <alejandro.penate@me.com>
2021-12-04 23:16:02 -0500

    still fixing append node view

--- OPS/Controllers/TreeViewController.cs
+++ OPS/Controllers/TreeViewController.cs
@@ -805,7 +805,7 @@ namespace OnlinePriceSystem.Controllers
             foreach (string n in node.Dependents) dep = dep + n + ";";
             NodeData nodedata = new NodeData(node.Name, node.Id, Expression, node.Url, node.CheckBox, node.Type.ToString(), node.Selected, node.IsComplete(), tree.Root.TotalStr, node.Optional, node.TotalStr, leaf, node.Hidden, node.ExpandedLevels, dep, EditChildren, node.Min.ToString(), node.Max.ToString(), node.Discount.ToString(), node.Order.ToString(), node.Report, node.ReportValue, node.Units, node.Parent != null && node.Parent.Type == NodeType.Decision, node.Template, node.HasErrors(), node.Error, node.ReadOnly, node.DisableCondition, node.Disabled, node.DisabledMessage);
 
-            TempData["tree"] = tree;
+            //TempData["tree"] = tree;
             string response = JsonConvert.SerializeObject(nodedata, Formatting.Indented);
             return Content(response);
         }
