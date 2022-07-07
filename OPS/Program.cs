edf6facb2ff0aacfa7a5b44e70cc56b4150fdecc
Alejandro Penate-Diaz <alejandro.penate@me.com>
2022-07-07 06:01:57 -0400

    hide the menu bar

--- OPS/Program.cs
+++ OPS/Program.cs
@@ -43,7 +43,8 @@ async void CreateElectronWindow()
     {
         Width = 1280,
         Height = 1024,
-        WebPreferences = new WebPreferences { WebSecurity = false}
+        WebPreferences = new WebPreferences { WebSecurity = false},
+        AutoHideMenuBar = true
     };
     //Electron.Dock.SetIcon("../../../../wwwroot/Images/electron.png");
     var window = await Electron.WindowManager.CreateWindowAsync(options);
