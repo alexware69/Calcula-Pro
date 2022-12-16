0e4e14b9b98453d3b11f3c480ca9eb6cc0d9dc1a
Alejandro <alejandro.penate@calculapro.com>
2022-12-16 01:09:45 -0500

    inserting spaces when autocomplete

--- OPS/wwwroot/js/TreeViewEditOne.js
+++ OPS/wwwroot/js/TreeViewEditOne.js
@@ -2199,6 +2199,8 @@ $(function () {
                     //terms.unshift( operator );
                     //this.value = this.value.replace(terms[terms.length -1],"")
                     this.value = this.value.replace(new RegExp(terms[terms.length -1] + '$'), '');
+                    if (ui.item.value[0] != " ") ui.item.value = " " + ui.item.value;
+                    if (ui.item.value[ui.item.value.length] != " ") ui.item.value = ui.item.value + " ";
                     this.value = this.value + ui.item.value;
                     return false;
                   }
