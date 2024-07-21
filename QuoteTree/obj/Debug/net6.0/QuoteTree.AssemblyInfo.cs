681b5035f824b9348664311ef2e8ec5fff73eb2c
Alejandro Penate-Diaz <alejandro.penate@calculapro.com>
2024-07-21 16:15:17 -0400

    text is now justify

--- QuoteTree/obj/Debug/net6.0/QuoteTree.AssemblyInfo.cs
+++ QuoteTree/obj/Debug/net6.0/QuoteTree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuoteTree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+148583332fecfb7459b114a67e7aec21f079f98f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63af2ecfd7abe228d4fe2e56d09191fe796e21b")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuoteTree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuoteTree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
